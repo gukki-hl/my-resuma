d79b324195eb534f59a7c67b03818b1979c213d2
陈海连 <948722262@qq.com>
2026-04-23 19:00:05 +0800

    feat：封装动画库

---
+++ app/components/home/client/AnimatedFeature.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+interface AnimatedFeatureProps {
+  children: React.ReactNode;
+  delay?: number;
+}
+
+const AnimatedFeature = ({ children, delay = 0 }: AnimatedFeatureProps) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.6, delay }}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export default AnimatedFeature;
